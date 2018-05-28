c40aaa1b6dd2718faa711bb0109a88ac627c973c
Fábio C. Barrionuevo da Luz <bnafta@gmail.com>
2018-05-28 14:12:40 -0300

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://cookiecutter-django-test2.xyz'
+SITEURL = 'http://pynorte.github.io/pyconamazonia2017-site'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
